edcfb99c23998cba48cfedbe22dedaaa374b28d0
alanlyne <alanlyne@outlook.com>
2020-11-09 21:14:05 +0000

    Fixed broken data link

--- Covid19-Tracker/src/main/java/com/alanlyne/Covid19Tracker/Services/CovidDataService.java
+++ Covid19-Tracker/src/main/java/com/alanlyne/Covid19Tracker/Services/CovidDataService.java
@@ -23,8 +23,7 @@ import org.springframework.stereotype.Service;
 
 public class CovidDataService {
 
-    private static String CovidDataURL = "https://opendata-geohive.hub.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D";
-
+    private static String CovidDataURL = "https://opendata.arcgis.com/datasets/d9be85b30d7748b5b7c09450b8aede63_0.csv";
     private List<Object> stats = new ArrayList<>();
 
     public List<Object> getStats() {
